b3d97596566165ab55880b9284cc452f908af6d9
kttkjl <jinglun.li@alumni.ubc.ca>
2019-07-25 23:30:38 -0700

    upgraded to express4, added script to kmk build

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var PORT = process.env["PORT"] || 5000;
+var PORT = process.env.REACT_APP_PORT || 5000;
+let DOMAIN = process.env.REACT_APP_DOMAIN || "http://localhost";
 
 var app = require("./lib/app.js")();
 
-console.log("Running @ http://localhost:" + PORT + ". Press ^C to Terminate.");
+console.log(`Running @ ${DOMAIN}:${PORT}. Press ^C to Terminate.`);
 app.listen(PORT);
